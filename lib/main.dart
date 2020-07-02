7a810df50e30bc3a18b04176bf257037390d0d59
Ankita Garg <gargankita9789@gmail.com>
2020-07-02 16:29:27 +0530

    Sign up image

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,70 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' ;
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    String name="Garima";
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-    return MaterialApp(
-      home: new Scaffold(
-        body:  SafeArea(
+    return new MaterialApp(
+      home: new SignUp(),
+    );
+  }
+}
+
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> with SingleTickerProviderStateMixin {
+AnimationController _iconAnimationController;
+Animation<double> _iconAnimation;
+
+@override
+void initState(){
+  super.initState();
+  _iconAnimationController = new AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500)
+  );
+  _iconAnimation = new CurvedAnimation(
+      parent: _iconAnimationController,
+      curve: Curves.bounceInOut
+  );
+  _iconAnimation.addListener(() => this.setState((){}));
+  _iconAnimationController.forward();
+}
+
+
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Colors.black,
+                  Colors.teal,
+                  Colors.black
+                ]
+            ),
+          ),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                width: 500.0,
-                height: 190.0,
-                color: Colors.red,
-                child:  Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(top: 15.0),
-                      child: Image(
-                        height: 100.0,
-                        image: AssetImage('images/logo_image.png'),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Text(
-                      "Welcome $name!",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'BreeSerif',
-                        fontSize: 25.0,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                height: 350.0,
-                width: 330.0,
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.redAccent[200],
-                    ),
-                  ],
-                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10.0), bottomRight: Radius.circular(10.0)),
-                ),
-                padding: EdgeInsets.only(left: 20.0,top: 20.0),
-                child: Column(
-                  children: <Widget>[
-                    Text("The age of your child is 2 years 4 months. According to this age, some games have been selected for your child. Once"
-                        " your child progresses "
-                        "in these games, we will automatically upgrade its level",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ],
-                ),
+              AnimatedBuilder(
+                animation: _iconAnimation,
+                builder: (BuildContext context, Widget child) {
+                  String frame = _iconAnimation.value.toString().padLeft(3,'0');
+                  return new Image.asset(
+                    'images/logo_image.png',
+                    gaplessPlayback: true,
+                  );
+                },
               ),
             ],
           ),
