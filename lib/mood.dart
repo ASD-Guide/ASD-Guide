397b0650cb1a6e5694e63b018d147143ebd5b35d
Garima Bansal <garima1051.cse18@chitkara.edu.in>
2020-07-04 18:40:42 +0530

    mood page added

---
+++ lib/mood.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Mood(),
+    );
+  }
+}
+
+class Mood extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<Mood> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Image(
+              image: AssetImage('images/bg.jpg'),
+              fit: BoxFit.cover,
+              colorBlendMode: BlendMode.darken,
+              color: Colors.black38,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top:50.0, left:150.0),
+              child: Container(
+                child: Text(
+                  "HELLO KIDDIE !",
+                  style: TextStyle(
+                    fontFamily: 'Forum',
+                    color: Colors.white,
+                    fontSize: 45.0,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top:200.0, left:40.0),
+              child: Text("How are you feeling?",
+                style: TextStyle(
+                  fontSize: 27.0,
+                  fontFamily: 'ZillaSlab',
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Positioned(
+              left: 30.0,
+              top:270.0,
+              child: Container(
+                width:300.0,
+                height: 300.0,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.white,
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 40.0,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Image(
+                          width:80.0,
+                          image: AssetImage('images/happy.png'),
+                        ),
+                        ButtonTheme(
+                          minWidth: 180.0,
+                          height: 50.0,
+                          child: RaisedButton(
+                            color: Colors.blueGrey[900],
+                            onPressed: (){},
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(topLeft: Radius.circular(10.0), bottomLeft: Radius.circular(10.0))),
+                            child: Text("Happy",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'BreeSerif',
+                                fontSize: 20.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 40.0,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        ButtonTheme(
+                          minWidth: 180.0,
+                          height: 50.0,
+                          child: RaisedButton(
+                            onPressed: (){},
+                            color: Colors.blueGrey[900],
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(topRight: Radius.circular(10.0), bottomRight: Radius.circular(10.0))),
+                            child: Text("Sad",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'BreeSerif',
+                                fontSize: 20.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Image(
+                          width: 80.0,
+                          image: AssetImage('images/sad.png'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
