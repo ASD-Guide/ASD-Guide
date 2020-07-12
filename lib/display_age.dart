42ee81623daae8962b1e13ed02592917a23ff021
Garima Bansal <garima1051.cse18@chitkara.edu.in>
2020-07-12 20:22:46 +0530

    final

--- lib/display_age.dart
+++ lib/display_age.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'mood.dart';
+import 'package:asdguide/mood.dart';
 
-
-class MyAge extends StatelessWidget {
-  // This widget is the root of your application.
+class MyAge extends StatelessWidget {// This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-    String name = "Garima";
     return MaterialApp(
       home: Display()
     );
@@ -29,103 +25,110 @@ class _State extends State<Display> {
       body: SafeArea(
         child: Stack(
           children: <Widget>[
-            Positioned(
-              bottom: 0.0,
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height:  MediaQuery.of(context).size.height* 0.6,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Colors.blue[700], Colors.black],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                  ),
+            Container(
+              color: Colors.black87,
+            ),
+            Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(30.0),
                 ),
-                child: Column(
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(top:80.0,left: 20.0,right: 20.0),
-                      child: Text("The age of your child is 2 years 5 months. According to this age some games have been"
-                          " selected for your child. Once your child progresses in these games, we will automatically upgrade "
-                          "its level.",
-                        style: TextStyle(
-                          fontSize: 21.7,
-                          fontFamily: 'Forum',
-                          color: Colors.white,
+                    Container(
+                      width: MediaQuery.of(context).size.width-60,
+                      height: MediaQuery.of(context).size.height-30-MediaQuery.of(context).size.height*0.6,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        gradient: LinearGradient(
+                          colors: [Colors.blue[700],Colors.black],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+
                         ),
                       ),
-                    ),
-                    SizedBox(height:20.0),
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: ButtonTheme(
-                        minWidth: 118.0,
-                        height: 50.0,
-                        child: RaisedButton(
-                          shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(45.0)),
-                          child: Text("Got it!",
-                            style: TextStyle(
-                              fontFamily: 'Forum',
-                              fontSize: 20.0,
-                              color: Colors.blue,
-                              fontWeight: FontWeight.bold
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(top:10.0),
+                            child: Image(
+                              height: 100.0,
+                              image: AssetImage(
+                                'images/logo_image.png',
+                              ),
                             ),
                           ),
-                          color: Colors.white,
-                          onPressed: (){
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => MyMood(),
-                              ),
-                            );
-                          },
-                        ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          Text("Welcome $name!",
+                            style: TextStyle(
+                              fontSize: 23.0,
+                              fontFamily: 'BreeSerif',
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
-              ),
-            ),
-            Positioned(
-              top: 50.0,
-              left: 30.0,
-              child: Container(
-                width: 300.0,
-                height: 230.0,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  gradient: LinearGradient(
-                    colors: [Colors.blue[700],Colors.black],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-
+                Container(
+                  height:  MediaQuery.of(context).size.height*0.5,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Colors.blue[700], Colors.black],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Text("The age of your child is 2 years 5 months. According to this age some games have been"
+                            " selected for your child. Once your child progresses in these games, we will automatically upgrade "
+                            "its level.",
+                          style: TextStyle(
+                            fontSize: 21.7,
+                            fontFamily: 'Forum',
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height:20.0),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: ButtonTheme(
+                          minWidth: 118.0,
+                          height: 50.0,
+                          child: RaisedButton(
+                            shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(45.0)),
+                            child: Text("Got it!",
+                              style: TextStyle(
+                                  fontFamily: 'Forum',
+                                  fontSize: 20.0,
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold
+                              ),
+                            ),
+                            color: Colors.white,
+                            onPressed: (){
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => MyMood(),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(top:10.0),
-                      child: Image(
-                        height: 100.0,
-                        image: AssetImage(
-                          'images/logo_image.png',
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    Text("Welcome $name!",
-                      style: TextStyle(
-                        fontSize: 23.0,
-                        fontFamily: 'BreeSerif',
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              ],
             ),
           ],
         ),
--- lib/mood.dart
+++ lib/mood.dart
@@ -2,17 +2,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:asdguide/options.dart';
-
+import 'package:auto_size_text/auto_size_text.dart';
 class MyMood extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Mood(),
-      routes: <String,WidgetBuilder>
-      {
-        "/b": (BuildContext context) => MyOptions(),
-      },
     );
   }
 }
@@ -38,99 +34,106 @@ class _State extends State<Mood> {
               color: Colors.black38,
             ),
             Padding(
-              padding: const EdgeInsets.only(top:50.0, left:150.0),
+              padding: const EdgeInsets.all(30.0),
               child: Container(
-                child: Text(
-                  "HELLO KIDDIE !",
-                  style: TextStyle(
-                    fontFamily: 'Forum',
-                    color: Colors.white,
-                    fontSize: 45.0,
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top:200.0, left:40.0),
-              child: Text("How are you feeling?",
-                style: TextStyle(
-                  fontSize: 27.0,
-                  fontFamily: 'ZillaSlab',
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            Positioned(
-              left: 30.0,
-              top:270.0,
-              child: Container(
-                width:300.0,
-                height: 300.0,
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.white,
-                    ),
-                  ],
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
-                    SizedBox(height: 40.0,),
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisAlignment: MainAxisAlignment.end,
                       children: <Widget>[
-                        Image(
-                          width:80.0,
-                          image: AssetImage('images/happy.png'),
-                        ),
-                        ButtonTheme(
-                          minWidth: 180.0,
-                          height: 50.0,
-                          child: RaisedButton(
-                            color: Colors.blueGrey[900],
-                            onPressed: (){},
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(topLeft: Radius.circular(10.0), bottomLeft: Radius.circular(10.0))),
-                            child: Text("Happy",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontFamily: 'BreeSerif',
-                                fontSize: 20.0,
-                              ),
-                            ),
+                        AutoSizeText(
+                          "HELLO\nKIDDIE!",
+                          style: TextStyle(
+                            fontFamily: 'Forum',
+                            color: Colors.white,
+                            fontSize: 45.0,
                           ),
                         ),
                       ],
                     ),
-                    SizedBox(height: 40.0,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        ButtonTheme(
-                          minWidth: 180.0,
-                          height: 50.0,
-                          child: RaisedButton(
-                            onPressed: (){
-                              Navigator.of(context).pushNamed("/b");
-                            },
-                            color: Colors.blueGrey[900],
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(topRight: Radius.circular(10.0), bottomRight: Radius.circular(10.0))),
-                            child: Text("Sad",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontFamily: 'BreeSerif',
-                                fontSize: 20.0,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Image(
-                          width: 80.0,
-                          image: AssetImage('images/sad.png'),
-                        ),
-                      ],
+                    Text("How are you feeling?",
+                      style: TextStyle(
+                        fontSize: 27.0,
+                        fontFamily: 'ZillaSlab',
+                        color: Colors.white,
+                      ),
                     ),
-                  ],
+                    Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.white,
+                          ),
+                        ],
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(height: 40.0,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Image(
+                                width:80.0,
+                                image: AssetImage('images/happy.png'),
+                              ),
+                              ButtonTheme(
+                                minWidth: 180.0,
+                                height: 50.0,
+                                child: RaisedButton(
+                                  color: Colors.blueGrey[900],
+                                  onPressed: (){},
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(topLeft: Radius.circular(10.0), bottomLeft: Radius.circular(10.0))),
+                                  child: Text("Happy",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'BreeSerif',
+                                      fontSize: 20.0,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 40.0,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              ButtonTheme(
+                                minWidth: 180.0,
+                                height: 50.0,
+                                child: RaisedButton(
+                                  onPressed: (){
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => MyOptions(),
+                                      ),
+                                    );
+                                  },
+                                  color: Colors.blueGrey[900],
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(topRight: Radius.circular(10.0), bottomRight: Radius.circular(10.0))),
+                                  child: Text("Sad",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'BreeSerif',
+                                      fontSize: 20.0,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Image(
+                                width: 80.0,
+                                image: AssetImage('images/sad.png'),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 40.0,),
+                        ],
+                      ),
+                    ),
+                  ]
                 ),
               ),
             ),
